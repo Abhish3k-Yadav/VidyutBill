088b9db84dbc5ee4be487d2873c5bf9afc75ec9c
Abhishek <abhiyadavacer@gmail.com>
2025-07-08 12:18:24 +0530

    payment  url

--- VidyutBill/src/vidyutbill/Paytm.java
+++ VidyutBill/src/vidyutbill/Paytm.java
@@ -1,46 +1,48 @@
 package vidyutbill;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
-public class Paytm extends JFrame implements ActionListener{
+public class Paytm extends JFrame implements ActionListener {
 
     String meter;
     JButton back;
+
     Paytm(String meter) {
         this.meter = meter;
-        
-        JEditorPane j = new JEditorPane();
-        j.setEditable(false);
-        
-        try {
-            j.setPage("https://paytm.com/online-payments");
-        } catch (Exception e) {
-            j.setContentType("text/html");
-            j.setText("<html>Could not load<html>");
-            
-        }
-        
-        JScrollPane pane = new JScrollPane(j);
-        add(pane);
-        
+        setLayout(new BorderLayout());
+
+        JLabel infoLabel = new JLabel("<html><center>For security, the payment page will open in your default browser.<br>After completing payment, return here and click 'Back'.</center></html>", SwingConstants.CENTER);
+        infoLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        infoLabel.setForeground(Color.RED);
+        add(infoLabel, BorderLayout.CENTER);
+
         back = new JButton("Back");
-        back.setBounds(640, 20, 80, 30);
+        back.setPreferredSize(new Dimension(80, 30));
         back.addActionListener(this);
-        j.add(back);
-        
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(back);
+        add(buttonPanel, BorderLayout.NORTH);
+
         setSize(800, 600);
-        setLocation(400, 150);
+        setLocation(400, 250);
         setVisible(true);
-        
+
+        // Open Paytm in browser
+        try {
+            Desktop.getDesktop().browse(new java.net.URI("https://paytm.com/electricity-bill-payment"));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Could not open browser. Please visit https://paytm.com/electricity-bill-payment manually.");
+        }
     }
-    
+
     public void actionPerformed(ActionEvent ae) {
         setVisible(false);
         new PayBill(meter);
     }
-    
+
     public static void main(String[] args) {
         new Paytm("");
     }
